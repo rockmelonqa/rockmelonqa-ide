238b25d871c39f97a3f9ae4127a4af8cb9a5b1cc
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-04-15 12:20:49 +0700

    Rename class; format code

--- electron/worker/actions/runTest/runNunitCommandBuilder.ts
+++ electron/worker/actions/runTest/runNunitCommandBuilder.ts
@@ -2,7 +2,7 @@ import { IRunTestSettings } from "rockmelonqa.common/ipc-defs/testRunner";
 import { ICommandBuilder } from "./commandBuilder";
 import RunDotnetTestCommandBuilder from "./runDotnetTestCommandBuilder";
 
-export default class RunMsTestCommandBuilder implements ICommandBuilder {
+export default class RunNunitCommandBuilder implements ICommandBuilder {
   build(settings: IRunTestSettings, resultFilePath: string) {
     let cmd = new RunDotnetTestCommandBuilder().build(settings, resultFilePath);
     return cmd;
