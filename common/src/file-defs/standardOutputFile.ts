9be82c6231b563c1025fef601cacb3a26a120e39
vothaison <vothaison@hotmail.com>
2023-04-29 12:00:30 +0700

    Add standard output file TestSuiteBase

--- common/src/file-defs/standardOutputFile.ts
+++ common/src/file-defs/standardOutputFile.ts
@@ -5,6 +5,7 @@ export class StandardOutputFile {
   static readonly PageDefinitions: string = "PageDefinitions";
   static readonly LocatorHelper: string = "LocatorHelper";
   static readonly TestCaseBase: string = "TestCaseBase";
+  static readonly TestSuiteBase: string = "TestSuiteBase";
   static readonly MetaData: string = ".code-metadata";
 
   // Nodejs
