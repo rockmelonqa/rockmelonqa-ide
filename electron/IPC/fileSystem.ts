a2fc960031ed0c6de40f99ad96fb6edc700e7616
vothaison <vothaison@hotmail.com>
2023-04-30 20:43:50 +0700

    Revert indentation; Add local prettierrc file;

--- electron/IPC/fileSystem.ts
+++ electron/IPC/fileSystem.ts
@@ -1,12 +1,12 @@
 import chokidar, { FSWatcher } from "chokidar";
 import { BrowserWindow, dialog } from "electron";
 import {
-    IAddFileWatchEventArgs,
-    IFileSystemInfo,
-    IGetFolderRequest,
-    IIpcResponse,
-    IRenameFileRequest,
-    IWriteFileRequest,
+  IAddFileWatchEventArgs,
+  IFileSystemInfo,
+  IGetFolderRequest,
+  IIpcResponse,
+  IRenameFileRequest,
+  IWriteFileRequest,
 } from "rockmelonqa.common";
 import * as fs from "../utils/fileSystem";
 import { IChannels } from "./core/channelsInterface";
@@ -16,29 +16,29 @@ const nameAPI = "fileSystem";
 
 // to Main
 const validSendChannel: IChannels = {
-    createFolder: createFolder,
-    watch: watch,
+  createFolder: createFolder,
+  watch: watch,
 };
 
 const validInvokeChannel: IChannels = {
-    getFolder: getFolder,
-    cloneFileSystem: cloneFileSystem,
-    deleteFileSystem: deleteFileSystem,
-    pickFolder: pickFolder,
-    readFile: readFile,
-    rename: rename,
-    walkFolder: walkFolder,
-    writeFile: writeFile,
+  getFolder: getFolder,
+  cloneFileSystem: cloneFileSystem,
+  deleteFileSystem: deleteFileSystem,
+  pickFolder: pickFolder,
+  readFile: readFile,
+  rename: rename,
+  walkFolder: walkFolder,
+  writeFile: writeFile,
 };
 
 // from Main
 const validReceiveChannel: string[] = ["watch:add", "watch:unlink", "watch:addDir", "watch:unlinkDir", "watch:change"];
 
 const fileSystem = new IPC({
-    nameAPI,
-    validSendChannel,
-    validInvokeChannel,
-    validReceiveChannel,
+  nameAPI,
+  validSendChannel,
+  validInvokeChannel,
+  validReceiveChannel,
 });
 
 /*
@@ -53,108 +53,108 @@ export default fileSystem;
  * Create new folder
  */
 async function createFolder(browserWindow: BrowserWindow, event: Electron.IpcMainEvent, path: string): Promise<void> {
-    await fs.createFoler(path);
+  await fs.createFoler(path);
 }
 
 /**
  * Delete file or folder at given path
  */
 async function deleteFileSystem(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    path: string
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  path: string
 ): Promise<IIpcResponse> {
-    try {
-        await fs.deleteFileSystem(path);
-        return { isSuccess: true } as IIpcResponse;
-    } catch (error: any) {
-        return { isSuccess: false, errorMessage: "Cannot delete file" } as IIpcResponse;
-    }
+  try {
+    await fs.deleteFileSystem(path);
+    return { isSuccess: true } as IIpcResponse;
+  } catch (error: any) {
+    return { isSuccess: false, errorMessage: "Cannot delete file" } as IIpcResponse;
+  }
 }
 /**
  * Delete file or folder at given path
  */
 async function cloneFileSystem(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    path: string
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  path: string
 ): Promise<IIpcResponse> {
-    try {
-        await fs.cloneFileSystem(path);
-        return { isSuccess: true } as IIpcResponse;
-    } catch (error: any) {
-        return { isSuccess: false, errorMessage: "Cannot clone file" } as IIpcResponse;
-    }
+  try {
+    await fs.cloneFileSystem(path);
+    return { isSuccess: true } as IIpcResponse;
+  } catch (error: any) {
+    return { isSuccess: false, errorMessage: "Cannot clone file" } as IIpcResponse;
+  }
 }
 
 /**
  * Get folder info
  */
 async function getFolder(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    message: IGetFolderRequest
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  message: IGetFolderRequest
 ): Promise<IFileSystemInfo | null> {
-    return await fs.getFolder(message.path, message.includeChildren);
+  return await fs.getFolder(message.path, message.includeChildren);
 }
 
 /**
  * Invoke OS folder picker to select a folder
  */
 async function pickFolder(browserWindow: BrowserWindow, event: Electron.IpcMainEvent): Promise<string | void> {
-    const { canceled, filePaths } = await dialog.showOpenDialog(browserWindow, { properties: ["openDirectory"] });
-    if (canceled) {
-        return;
-    } else {
-        return filePaths[0];
-    }
+  const { canceled, filePaths } = await dialog.showOpenDialog(browserWindow, { properties: ["openDirectory"] });
+  if (canceled) {
+    return;
+  } else {
+    return filePaths[0];
+  }
 }
 
 /**
  * Read file content as text
  */
 async function readFile(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    filePath: string
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  filePath: string
 ): Promise<string | null> {
-    try {
-        return await fs.readFile(filePath);
-    } catch (error) {
-        return null;
-    }
+  try {
+    return await fs.readFile(filePath);
+  } catch (error) {
+    return null;
+  }
 }
 
 /**
  * Rename file system
  */
 async function rename(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    data: IRenameFileRequest
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  data: IRenameFileRequest
 ): Promise<IIpcResponse> {
-    try {
-        await fs.rename(data.oldPath, data.newPath);
-        return { isSuccess: true } as IIpcResponse;
-    } catch (error: any) {
-        return { isSuccess: false, errorMessage: "Cannot rename file" } as IIpcResponse;
-    }
+  try {
+    await fs.rename(data.oldPath, data.newPath);
+    return { isSuccess: true } as IIpcResponse;
+  } catch (error: any) {
+    return { isSuccess: false, errorMessage: "Cannot rename file" } as IIpcResponse;
+  }
 }
 
 /**
  * Get children file system (folder/file) of given path
  */
 async function walkFolder(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    folderPath: string
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  folderPath: string
 ): Promise<IIpcResponse> {
-    try {
-        const files = await fs.walkFolder(folderPath);
-        return { isSuccess: true, data: files } as IIpcResponse;
-    } catch (error) {
-        return { isSuccess: false, errorMessage: "Cannot walk folder" };
-    }
+  try {
+    const files = await fs.walkFolder(folderPath);
+    return { isSuccess: true, data: files } as IIpcResponse;
+  } catch (error) {
+    return { isSuccess: false, errorMessage: "Cannot walk folder" };
+  }
 }
 
 // Singleton watcher (ensure only one watcher object is being used),
@@ -165,48 +165,48 @@ let fsWatcher: FSWatcher;
  * Watch changes at specific file or folder
  */
 async function watch(browserWindow: BrowserWindow, event: Electron.IpcMainEvent, path: string): Promise<void> {
-    if (fsWatcher != null) {
-        await fsWatcher.close();
-    }
+  if (fsWatcher != null) {
+    await fsWatcher.close();
+  }
 
-    let isReady = false;
-    fsWatcher = chokidar.watch(path);
-    fsWatcher
-        .on("add", function (path) {
-            browserWindow.webContents.send("watch:add", {
-                path,
-                isReady,
-            } as IAddFileWatchEventArgs);
-        })
-        .on("unlink", function (path) {
-            if (isReady) {
-                browserWindow.webContents.send("watch:unlink", path);
-            }
-        })
-        .on("addDir", function (path) {
-            if (isReady) {
-                browserWindow.webContents.send("watch:addDir", path);
-            }
-        })
-        .on("unlinkDir", function (path) {
-            if (isReady) {
-                browserWindow.webContents.send("watch:unlinkDir", path);
-            }
-        })
-        .on("change", function (path) {
-            if (isReady) {
-                browserWindow.webContents.send("watch:change", path);
-            }
-        })
-        .on("ready", function () {
-            isReady = true;
-        });
+  let isReady = false;
+  fsWatcher = chokidar.watch(path);
+  fsWatcher
+    .on("add", function (path) {
+      browserWindow.webContents.send("watch:add", {
+        path,
+        isReady,
+      } as IAddFileWatchEventArgs);
+    })
+    .on("unlink", function (path) {
+      if (isReady) {
+        browserWindow.webContents.send("watch:unlink", path);
+      }
+    })
+    .on("addDir", function (path) {
+      if (isReady) {
+        browserWindow.webContents.send("watch:addDir", path);
+      }
+    })
+    .on("unlinkDir", function (path) {
+      if (isReady) {
+        browserWindow.webContents.send("watch:unlinkDir", path);
+      }
+    })
+    .on("change", function (path) {
+      if (isReady) {
+        browserWindow.webContents.send("watch:change", path);
+      }
+    })
+    .on("ready", function () {
+      isReady = true;
+    });
 }
 
 async function writeFile(
-    browserWindow: BrowserWindow,
-    event: Electron.IpcMainEvent,
-    data: IWriteFileRequest
+  browserWindow: BrowserWindow,
+  event: Electron.IpcMainEvent,
+  data: IWriteFileRequest
 ): Promise<void> {
-    await fs.writeFile(data.filePath, data.fileContent);
+  await fs.writeFile(data.filePath, data.fileContent);
 }
--- electron/utils/fileSystem.ts
+++ electron/utils/fileSystem.ts
@@ -9,90 +9,90 @@ import { IFileSystemInfo } from "rockmelonqa.common";
  * @returns
  */
 export async function checkExists(filePath: string): Promise<boolean> {
-    try {
-        await fs.promises.access(filePath);
-        return true;
-    } catch (error) {
-        return false;
-    }
+  try {
+    await fs.promises.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 export function checkExistsSync(filePath: string): boolean {
-    return fs.existsSync(filePath);
+  return fs.existsSync(filePath);
 }
 
 /**
  * Create new folder
  */
 export async function createFoler(folderPath: string): Promise<void> {
-    let folderExists = await checkExists(folderPath);
-    if (!folderExists) {
-        try {
-            await fs.promises.mkdir(folderPath);
-        } catch (error) {
-            console.log("CANNOT create folder:", folderPath);
-            console.error(error);
-            throw error;
-        }
+  let folderExists = await checkExists(folderPath);
+  if (!folderExists) {
+    try {
+      await fs.promises.mkdir(folderPath);
+    } catch (error) {
+      console.log("CANNOT create folder:", folderPath);
+      console.error(error);
+      throw error;
     }
+  }
 }
 
 /**
  * Delete file or folder at given path
  */
 export async function deleteFileSystem(fullPath: string): Promise<void> {
-    try {
-        await fs.promises.rm(fullPath, { recursive: true, force: true });
-    } catch (error) {
-        console.log("CANNOT delete file:", fullPath);
-        console.error(error);
-        throw error;
-    }
+  try {
+    await fs.promises.rm(fullPath, { recursive: true, force: true });
+  } catch (error) {
+    console.log("CANNOT delete file:", fullPath);
+    console.error(error);
+    throw error;
+  }
 }
 
 /** Generates cloned file name when pasting the same file at nth time */
 const genClonedFileNameAtNth = (srcFileNameWithoutExt: string, ext: string, n: number) => {
-    if (n === 1) {
-        return `${srcFileNameWithoutExt} - Copy${ext}`;
-    }
-    return `${srcFileNameWithoutExt} - Copy (${n})${ext}`;
+  if (n === 1) {
+    return `${srcFileNameWithoutExt} - Copy${ext}`;
+  }
+  return `${srcFileNameWithoutExt} - Copy (${n})${ext}`;
 };
 
 /**
  * Copies file at given path and paste it the same folder, i.e clones it.
  */
 export async function cloneFileSystem(fullPath: string): Promise<void> {
-    try {
-        const srcFileNameWithoutExt = path.parse(fullPath).name;
-        const srcFileExt = path.extname(fullPath);
-        const srcDir = path.dirname(fullPath);
+  try {
+    const srcFileNameWithoutExt = path.parse(fullPath).name;
+    const srcFileExt = path.extname(fullPath);
+    const srcDir = path.dirname(fullPath);
 
-        // Use Windows Explorer file copy/paste naming pattern:
-        // EX:
-        // - Src file name:             Document.txt
-        // - First copied file name:    Document - Copy.txt
-        // - Second copied file name:   Document - Copy (2).txt
-        // - etc...
+    // Use Windows Explorer file copy/paste naming pattern:
+    // EX:
+    // - Src file name:             Document.txt
+    // - First copied file name:    Document - Copy.txt
+    // - Second copied file name:   Document - Copy (2).txt
+    // - etc...
 
-        let foundExistingClonedFile = false;
-        let pastingTime = 1;
-        while (!foundExistingClonedFile) {
-            let newClonedFileName = genClonedFileNameAtNth(srcFileNameWithoutExt, srcFileExt, pastingTime);
-            let newClonedFilePath = path.join(srcDir, newClonedFileName);
-            let existing = fs.existsSync(newClonedFilePath);
-            if (!existing) {
-                // Create the file
-                const newFileName = await fse.copy(fullPath, newClonedFilePath);
-                foundExistingClonedFile = true;
-                return;
-            }
-            pastingTime += 1;
-        }
-    } catch (error) {
-        console.log("CANNOT clone file:", fullPath);
-        console.error(error);
-        throw error;
+    let foundExistingClonedFile = false;
+    let pastingTime = 1;
+    while (!foundExistingClonedFile) {
+      let newClonedFileName = genClonedFileNameAtNth(srcFileNameWithoutExt, srcFileExt, pastingTime);
+      let newClonedFilePath = path.join(srcDir, newClonedFileName);
+      let existing = fs.existsSync(newClonedFilePath);
+      if (!existing) {
+        // Create the file
+        const newFileName = await fse.copy(fullPath, newClonedFilePath);
+        foundExistingClonedFile = true;
+        return;
+      }
+      pastingTime += 1;
     }
+  } catch (error) {
+    console.log("CANNOT clone file:", fullPath);
+    console.error(error);
+    throw error;
+  }
 }
 
 /**
@@ -100,28 +100,28 @@ export async function cloneFileSystem(fullPath: string): Promise<void> {
  * Always determine 'hasChildren', but optionally load 'children'
  */
 export async function getFolder(folderPath: string, includeChildren?: boolean): Promise<IFileSystemInfo | null> {
-    const stats = await fs.promises.lstat(folderPath);
-    if (!stats.isDirectory()) {
-        return null;
-    }
+  const stats = await fs.promises.lstat(folderPath);
+  if (!stats.isDirectory()) {
+    return null;
+  }
 
-    let children: IFileSystemInfo[] | undefined;
-    let hasChildren: boolean;
+  let children: IFileSystemInfo[] | undefined;
+  let hasChildren: boolean;
 
-    if (includeChildren) {
-        children = await walkFolder(folderPath);
-        hasChildren = children.length > 0;
-    } else {
-        hasChildren = !(await isEmptyFolder(folderPath));
-    }
+  if (includeChildren) {
+    children = await walkFolder(folderPath);
+    hasChildren = children.length > 0;
+  } else {
+    hasChildren = !(await isEmptyFolder(folderPath));
+  }
 
-    return {
-        path: folderPath,
-        name: path.basename(folderPath),
-        isDir: true,
-        hasChildren,
-        children,
-    } as IFileSystemInfo;
+  return {
+    path: folderPath,
+    name: path.basename(folderPath),
+    isDir: true,
+    hasChildren,
+    children,
+  } as IFileSystemInfo;
 }
 
 /**
@@ -130,15 +130,15 @@ export async function getFolder(folderPath: string, includeChildren?: boolean): 
  * @returns
  */
 export async function isEmptyFolder(folderPath: string): Promise<boolean> {
-    try {
-        const folder = await fs.promises.opendir(folderPath);
-        const entry = await folder.read();
-        await folder.close();
+  try {
+    const folder = await fs.promises.opendir(folderPath);
+    const entry = await folder.read();
+    await folder.close();
 
-        return entry === null;
-    } catch (error) {
-        return false;
-    }
+    return entry === null;
+  } catch (error) {
+    return false;
+  }
 }
 
 /**
@@ -147,30 +147,30 @@ export async function isEmptyFolder(folderPath: string): Promise<boolean> {
  * @returns
  */
 export async function readFile(filePath: string): Promise<string> {
-    try {
-        return await fs.promises.readFile(filePath, "utf-8");
-    } catch (error) {
-        console.log("CANNOT read file:", filePath);
-        console.error(error);
-        throw error;
-    }
+  try {
+    return await fs.promises.readFile(filePath, "utf-8");
+  } catch (error) {
+    console.log("CANNOT read file:", filePath);
+    console.error(error);
+    throw error;
+  }
 }
 
 /**
  * Rename file system
  */
 export async function rename(oldPath: string, newPath: string): Promise<void> {
-    if (await checkExists(newPath)) {
-        throw new Error(`'${newPath}' already exists`);
-    }
+  if (await checkExists(newPath)) {
+    throw new Error(`'${newPath}' already exists`);
+  }
 
-    try {
-        await fs.promises.rename(oldPath, newPath);
-    } catch (error) {
-        console.log(`CANNOT rename from '${oldPath}' to '${newPath}'`);
-        console.error(error);
-        throw error;
-    }
+  try {
+    await fs.promises.rename(oldPath, newPath);
+  } catch (error) {
+    console.log(`CANNOT rename from '${oldPath}' to '${newPath}'`);
+    console.error(error);
+    throw error;
+  }
 }
 
 /**
@@ -179,48 +179,48 @@ export async function rename(oldPath: string, newPath: string): Promise<void> {
  * @param fileContent
  */
 export async function writeFile(filePath: string, fileContent: string): Promise<void> {
-    try {
-        await fs.promises.writeFile(filePath, fileContent);
-    } catch (error) {
-        console.log("CANNOT write file:", filePath);
-        console.error(error);
-        throw error;
-    }
+  try {
+    await fs.promises.writeFile(filePath, fileContent);
+  } catch (error) {
+    console.log("CANNOT write file:", filePath);
+    console.error(error);
+    throw error;
+  }
 }
 
 /**
  * Get children file system (folder/file) of given path
  */
 export async function walkFolder(folderPath: string): Promise<IFileSystemInfo[]> {
-    try {
-        let results: IFileSystemInfo[] = [];
+  try {
+    let results: IFileSystemInfo[] = [];
 
-        const files = await fs.promises.readdir(folderPath);
-        for (const file of files) {
-            const pathToFile = path.join(folderPath, file);
-            try {
-                const stat = await fs.promises.stat(pathToFile);
-                const isDirectory = stat.isDirectory();
+    const files = await fs.promises.readdir(folderPath);
+    for (const file of files) {
+      const pathToFile = path.join(folderPath, file);
+      try {
+        const stat = await fs.promises.stat(pathToFile);
+        const isDirectory = stat.isDirectory();
 
-                results.push({
-                    path: pathToFile,
-                    name: file,
-                    isDir: isDirectory,
-                    hasChildren: isDirectory ? !(await isEmptyFolder(pathToFile)) : false,
-                } as IFileSystemInfo);
-            } catch (err) {
-                results.push({
-                    path: pathToFile,
-                    name: file,
-                    error: err,
-                } as IFileSystemInfo);
-            }
-        }
-
-        return results;
-    } catch (error) {
-        console.log("CANNOT walkFolder:", folderPath);
-        console.error(error);
-        throw error;
+        results.push({
+          path: pathToFile,
+          name: file,
+          isDir: isDirectory,
+          hasChildren: isDirectory ? !(await isEmptyFolder(pathToFile)) : false,
+        } as IFileSystemInfo);
+      } catch (err) {
+        results.push({
+          path: pathToFile,
+          name: file,
+          error: err,
+        } as IFileSystemInfo);
+      }
     }
+
+    return results;
+  } catch (error) {
+    console.log("CANNOT walkFolder:", folderPath);
+    console.error(error);
+    throw error;
+  }
 }
