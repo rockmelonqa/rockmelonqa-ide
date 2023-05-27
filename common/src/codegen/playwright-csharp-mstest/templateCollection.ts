964cb008878abd7f6b2b2b5e939bdb48b73dcde8
vothaison <vothaison@hotmail.com>
2023-05-27 14:46:09 +0700

    Better names for templates

--- common/src/codegen/playwright-csharp-mstest/templateCollection.ts
+++ common/src/codegen/playwright-csharp-mstest/templateCollection.ts
@@ -1,40 +1,42 @@
 import { compile } from "handlebars";
 import { loadTemplate } from "../utils/templateLoader";
 
-/** Templates collection for Playwright Csharp MsTest codegen*/
+/** Templates collection for Playwright Csharp MsTest codegen */
 export class MsTestTemplateCollection {
-  public PAGE_DEFINITIONS_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public PAGE_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public LOCATOR_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public TEST_FUNCTION_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public TEST_CASE_FILE_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public TEST_ROUTINE_FILE_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public TEST_SUITE_FILE: HandlebarsTemplateDelegate<any>;
-  public TEST_SUITE_BASE: HandlebarsTemplateDelegate<any>;
-  public LOCATOR_HELPER_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public CSPROJECT_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public USINGS_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public RUNSETTINGS_TEMPLATE: HandlebarsTemplateDelegate<any>;
-  public COMMENT: HandlebarsTemplateDelegate<any>;
-  public TEST_CASE_BASE: HandlebarsTemplateDelegate<any>;
+  public readonly COMMENT: HandlebarsTemplateDelegate<any>;
+  public readonly CSPROJECT_TEMPLATE: HandlebarsTemplateDelegate<any>;
+  public readonly LOCATOR_HELPER_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly PAGE_DEFINITIONS_TEMPLATE: HandlebarsTemplateDelegate<any>;
+  public readonly PAGE_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly PAGE_ELEMENT_PROPERTY: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_FUNCTION_TEMPLATE: HandlebarsTemplateDelegate<any>;
+  public readonly RUNSETTINGS_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_CASE_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_SUITE_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_SUITE_BASE_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly USINGS_TEMPLATE: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_CASE_BASE_FILE: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_ROUTINE_CLASS: HandlebarsTemplateDelegate<any>;
+  public readonly TEST_ROUTINE_FILE: HandlebarsTemplateDelegate<any>;
 
   constructor(templatesDir: string, customTemplatesDir: string, fileExtension: string) {
     const loadAndCompile = (templateFileName: string) =>
       compile(loadTemplate(templatesDir, customTemplatesDir, templateFileName + fileExtension));
 
+    this.COMMENT = loadAndCompile("Comment");
     this.PAGE_DEFINITIONS_TEMPLATE = loadAndCompile("PageDefinitions");
-    this.PAGE_TEMPLATE = loadAndCompile("Page");
-    this.LOCATOR_TEMPLATE = loadAndCompile("Locator");
+    this.PAGE_FILE = loadAndCompile("PageFile");
+    this.PAGE_ELEMENT_PROPERTY = loadAndCompile("PageElementProperty");
     this.TEST_FUNCTION_TEMPLATE = loadAndCompile("TestFunction");
-    this.TEST_CASE_FILE_TEMPLATE = loadAndCompile("TestCaseFile");
-    this.TEST_ROUTINE_FILE_TEMPLATE = loadAndCompile("TestRoutineFile");
-    this.TEST_SUITE_BASE = loadAndCompile("TestSuiteBase");
+    this.TEST_CASE_FILE = loadAndCompile("TestCaseFile");
+    this.TEST_ROUTINE_CLASS = loadAndCompile("TestRoutineClass");
+    this.TEST_ROUTINE_FILE = loadAndCompile("TestRoutineFile");
+    this.TEST_SUITE_BASE_FILE = loadAndCompile("TestSuiteBaseFile");
     this.TEST_SUITE_FILE = loadAndCompile("TestSuiteFile");
-    this.LOCATOR_HELPER_TEMPLATE = loadAndCompile("LocatorHelper");
+    this.LOCATOR_HELPER_FILE = loadAndCompile("LocatorHelperFile");
     this.CSPROJECT_TEMPLATE = loadAndCompile("CsProject");
     this.USINGS_TEMPLATE = loadAndCompile("Usings");
-    this.RUNSETTINGS_TEMPLATE = loadAndCompile("RunSettings");
-    this.COMMENT = loadAndCompile("Comment");
-    this.TEST_CASE_BASE = loadAndCompile("TestCaseBase");
+    this.RUNSETTINGS_FILE = loadAndCompile("RunSettingsFile");
+    this.TEST_CASE_BASE_FILE = loadAndCompile("TestCaseBaseFile");
   }
 }
