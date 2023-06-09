7c7b43ff0e7bde468c09434111f6f615ca22f5b5
Khanh Van <vquockhanh86@gmail.com>
2023-06-09 11:50:25 +0700

    Remove flowbite package (not use)

--- svelte/tailwind.config.cjs
+++ svelte/tailwind.config.cjs
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {
-      minWidth: {
-        64: '16rem',
-      },
-    },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('flowbite/plugin')
   ],
-  darkMode: 'class',
 }
