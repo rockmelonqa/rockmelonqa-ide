0b6c79977ea16bab85fea24445ee0afcdcba5241
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-06-13 21:07:35 +0700

    Issue 133: Add Standard folder `Config`

--- common/src/file-defs/standardFolder.ts
+++ common/src/file-defs/standardFolder.ts
@@ -2,6 +2,7 @@ export class StandardFolder {
   static readonly CustomCode: string = "custom-code";
   static readonly OutputCode: string = "output-code";
   static readonly Logs: string = "logs";
+  static readonly Config: string = "config";
   static readonly PageDefinitions: string = "page-definitions";
   static readonly TestRoutines: string = "test-routines";
   static readonly TestCases: string = "test-cases";
