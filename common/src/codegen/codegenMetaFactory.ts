9231ec7039fc6d3413955a0415e75f834b14e4ef
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-07-01 11:27:21 +0700

    Refactor code: lookup CodeGen, CodeGenMeta

--- common/src/codegen/codegenMetaFactory.ts
+++ common/src/codegen/codegenMetaFactory.ts
@@ -1,33 +1,35 @@
-import { AutomationFramework, ISourceProjectMetadata, TestFramework } from "../file-defs";
+import { AutomationFramework, ISourceProjectMetadata, Language, TestFramework } from "../file-defs";
+import { LookupKey, isEqual } from "./codegenFactory";
 import { IOutputProjectMetadataProcessor } from "./playwright-charp-common/outputProjectMetadataProcessor";
 import { MsTestProjMeta } from "./playwright-csharp-mstest/msTestProjMeta";
 import { NunitProjectMeta } from "./playwright-csharp-nunit/nunitProjectMeta";
 import { XUnitProjectMeta } from "./playwright-csharp-xunit/xunitProjectMeta";
 
-const codegenMetaRegisty: { [key in keyof typeof AutomationFramework]?: { [key in keyof typeof TestFramework]?: any } } = {
-  [AutomationFramework.Playwright]: {
-    [TestFramework.NUnit]: NunitProjectMeta,
-    [TestFramework.MSTest]: MsTestProjMeta,
-    [TestFramework.xUnit]: XUnitProjectMeta,
-    // ... add other types of TestFramework
-  },
-  // ... add other types of AutomationFramework
-};
+const registry = new Map<LookupKey, (projMeta: ISourceProjectMetadata) => IOutputProjectMetadataProcessor>();
+
+registry.set([AutomationFramework.Playwright, Language.CSharp, TestFramework.MSTest], (projMeta) => new MsTestProjMeta(projMeta));
+
+registry.set([AutomationFramework.Playwright, Language.CSharp, TestFramework.NUnit], (projMeta) => new NunitProjectMeta(projMeta));
+
+registry.set([AutomationFramework.Playwright, Language.CSharp, TestFramework.xUnit], (projMeta) => new XUnitProjectMeta(projMeta));
+
+//registry.set([AutomationFramework.Playwright, Language.Typescript, undefined], (projMeta) => new PlaywrightCsharpXUnitCodeGen(projMeta));
 
 export class CodeGenMetaFactory {
   /** Creates new instance of Codegen Meta Generator based on  { automationFramework, testFramework }*/
   static newInstance(projMeta: ISourceProjectMetadata): IOutputProjectMetadataProcessor {
-    let automationFramework = codegenMetaRegisty[projMeta.project.content.automationFramework];
+    const { automationFramework, language, testFramework } = projMeta.project.content;
+    const keyToLookup: LookupKey = [automationFramework, language, testFramework];
 
-    if (automationFramework === undefined) {
-      throw new Error(`Can't find Codegen Meta for AutomationFramework '${projMeta.project.content.automationFramework}'`);
+    for (const [key, value] of registry) {
+      if (isEqual(keyToLookup, key)) {
+        const codegen = value(projMeta);
+        return codegen;
+      }
     }
 
-    let codegenMetaClass = automationFramework[projMeta.project.content.testFramework];
-    if (!codegenMetaClass) {
-      throw new Error(`Can't find Codegen Meta class for TestFramework '${projMeta.project.content.testFramework}'`);
-    }
-
-    return new codegenMetaClass(projMeta);
+    throw new Error(
+      `DEV ERROR: Can't find MetadataProcessor class for [AutomationFramework ${automationFramework}, Language: ${language}, TestFramework: ${projMeta.project.content.testFramework}] '`
+    );
   }
 }
