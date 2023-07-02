538a3edaa5049dcbbf1a948e7a8f295906045b7b
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-07-02 09:29:41 +0700

    Add comment

--- common/src/codegen/utils/languageExtensionMap.ts
+++ common/src/codegen/utils/languageExtensionMap.ts
@@ -1,5 +1,6 @@
 import { Language } from "../../file-defs";
 
+/** Contains mappings from Language to file extension string. E.g CSharp-> ".cs"; TypeScript=>".ts", etc...*/
 export const languageExtensionMap: { [key in Language]: string } = {
   [Language.CSharp]: ".cs",
   [Language.Java]: ".java",
