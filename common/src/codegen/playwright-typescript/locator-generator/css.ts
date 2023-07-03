1917d5148251465114a6768284e341f95fe591c6
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-07-03 13:33:40 +0700

    Issue 22: Locators for playwright typescript

--- common/src/codegen/playwright-typescript/locator-generator/css.ts
+++ common/src/codegen/playwright-typescript/locator-generator/css.ts
@@ -2,8 +2,6 @@ import { ILocatorTemplateParam } from "../../types";
 import { escapeStr } from "../../utils/stringUtils";
 
 export default (params: ILocatorTemplateParam) => {
-  const { locatorStr, hasParams } = params;
-  return hasParams
-    ? `this._page.locator(\"${escapeStr(locatorStr)}\".format(...parameters}))`
-    : `this._page.locator(\"${escapeStr(locatorStr)}\")`;
+  const { locatorStr } = params;
+  return `this.page.locator("${escapeStr(locatorStr)}")`;
 };
