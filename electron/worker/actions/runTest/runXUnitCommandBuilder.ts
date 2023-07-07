ba2b520aa4920f8ea3178bc760fe2c4bbcd656fc
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-07-07 10:54:27 +0700

    Format code

--- electron/worker/actions/runTest/runXUnitCommandBuilder.ts
+++ electron/worker/actions/runTest/runXUnitCommandBuilder.ts
@@ -1,21 +1,26 @@
-import { IRunTestSettings } from 'rockmelonqa.common/ipc-defs/testRunner';
-import { ICommandBuilder } from './commandBuilder';
-import fs from 'fs';
-import path from 'path';
-import { RunSettingsFile } from './runsettingFile';
-import convert from 'xml-js';
-import { Platform, StandardOutputFile } from 'rockmelonqa.common/file-defs';
-import { IInvokeEnvironmentFileCmdBuilder, UnixInvokeEnvironmentFileCmdBuilder, WindowsInvokeEnvironmentFileCmdBuilder } from './invokeEnvironmentFileCmdBuilder';
+import { IRunTestSettings } from "rockmelonqa.common/ipc-defs/testRunner";
+import { ICommandBuilder } from "./commandBuilder";
+import fs from "fs";
+import path from "path";
+import { RunSettingsFile } from "./runsettingFile";
+import convert from "xml-js";
+import { Platform, StandardOutputFile } from "rockmelonqa.common/file-defs";
+import {
+  IInvokeEnvironmentFileCmdBuilder,
+  UnixInvokeEnvironmentFileCmdBuilder,
+  WindowsInvokeEnvironmentFileCmdBuilder,
+} from "./invokeEnvironmentFileCmdBuilder";
 const XML_OPTIONS = { compact: true, ignoreComment: true, spaces: 2 };
 
 export default class RunXUnitCommandBuilder implements ICommandBuilder {
-
-  private readonly invokeEnvironmentFileCmdBuilder: IInvokeEnvironmentFileCmdBuilder
+  private readonly invokeEnvironmentFileCmdBuilder: IInvokeEnvironmentFileCmdBuilder;
 
   constructor() {
-    this.invokeEnvironmentFileCmdBuilder = Platform.IsWindows() ? new WindowsInvokeEnvironmentFileCmdBuilder() : new UnixInvokeEnvironmentFileCmdBuilder()
+    this.invokeEnvironmentFileCmdBuilder = Platform.IsWindows()
+      ? new WindowsInvokeEnvironmentFileCmdBuilder()
+      : new UnixInvokeEnvironmentFileCmdBuilder();
   }
-  
+
   build(settings: IRunTestSettings, resultFilePath: string) {
     const commands = [];
 
@@ -31,11 +36,11 @@ export default class RunXUnitCommandBuilder implements ICommandBuilder {
     const settingXml = convert.json2xml(settingJson, XML_OPTIONS);
     fs.writeFileSync(path.join(settings.outputCodeDir, StandardOutputFile.RunSettings), settingXml);
 
-    const filterStr = settings.dotnetFilterStr ? `--filter "${settings.dotnetFilterStr}"` : '';
+    const filterStr = settings.dotnetFilterStr ? `--filter "${settings.dotnetFilterStr}"` : "";
 
     commands.push(`dotnet test ${filterStr} -l:"trx;LogFileName=${`..${path.sep}..${path.sep}${resultFilePath}`}"`);
 
-    const cmd = commands.join(' && ');
+    const cmd = commands.join(" && ");
 
     return cmd;
   }
