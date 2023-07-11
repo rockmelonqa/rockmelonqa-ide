f6a017e11a69e05c607d425dbd86579dfb46f918
Khanh Van <vquockhanh86@gmail.com>
2023-07-11 10:46:32 +0700

    Improve xunit and fix trx viewer

--- svelte/src/lib/components/Editors/HtmlViewer.svelte
+++ svelte/src/lib/components/Editors/HtmlViewer.svelte
@@ -2,15 +2,7 @@
     import { appContextKey, type IAppContext } from "$lib/context/AppContext";
     import { uiContextKey, type IUiContext } from "$lib/context/UiContext";
     import { fileSystem } from "$lib/ipc";
-    import type monaco from "monaco-editor";
-    import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-    import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-    import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-    import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-    import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-    import { StandardOutputFile } from "rockmelonqa.common/file-defs";
     import { createEventDispatcher, getContext, onMount } from "svelte";
-    import { appActionContextKey, type IAppActionContext } from "../Application";
     import { combinePath } from "../FileExplorer/Node";
 
     const uiContext = getContext(uiContextKey) as IUiContext;
@@ -30,4 +22,4 @@
     });
 </script>
 
-<iframe class="w-full h-full border-0" bind:this={frameEl} src="javascript:void(0);" />
+<iframe title="" class="w-full h-full border-0" bind:this={frameEl} src="javascript:void(0);" />
