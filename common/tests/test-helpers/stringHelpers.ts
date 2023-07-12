6bea0d6cb630116fc76a227032d7d28c5a701277
son.thai.vo <son.thai.vo@steadfasttech.com.au>
2023-07-12 08:14:48 +0700

    Add comment

--- common/tests/test-helpers/stringHelpers.ts
+++ common/tests/test-helpers/stringHelpers.ts
@@ -1,3 +1,4 @@
 import { EOL } from "os";
 
+/** Normalizes line endings of a string, i.e replace any line endings characters with current system EOL*/
 export const normalizeLineEndings = (str: string, normalized: string = EOL) => str.replace(/\r?\n/g, normalized);
