ec68b0bfdf64bcb3e3c53f11e35422d9c96dbaf5
son.thai.vo <son.thai.vo@steadfasttech.com.au>
2023-07-12 08:43:01 +0700

    Add comments to unit tests

--- common/tests/codegen-tests/templates-tests/templateReindenter.test.ts
+++ common/tests/codegen-tests/templates-tests/templateReindenter.test.ts
@@ -11,13 +11,23 @@ import { execSync } from "child_process";
 import { TemplateReindenter } from "../../../src/codegen/playwright-csharp-mstest/templateCollection";
 const tempDir = path.resolve(__dirname, "../../.tmp");
 
-// Notation
+// ####
+// # Test the conversion of indentations
+// ####
+
+// Concept:
+// An indentation of a file is specified with 2 values:
+// - IndentChar: the character that is used as indentation, could be whitespaces or tabs
+// - IndentSize: the size of the indent, could be any integer value, 1 , 2 , 4,...
+// Notation:
+// In these tests, we use this notation to indicate indentation of a file
 // {IndentChar}/{IndentSize}
 // EX
 // - {Spaces}/{4}
 // - {Spaces}/{2}
 // - {Tabs}/{1}
 
+/** Contains sample source code with different indentation. We then have some tests for converting one indentation to another */
 const SOURCE = new Map<string, string>();
 SOURCE.set(
   "{Spaces}/{4}",
@@ -82,30 +92,35 @@ public partial class {{{testCaseName}}} : TestCaseBase
 }`.trimStart()
 );
 
+/** The collection of test cases.
+ * Each case is a tupple of [ConvertFrom, ConvertTo] to indicate that we want to convert one indentation to another.
+ * (we might wanna use a "permutation" generator instead of listing the tupples manually) */
 const cases = [
-  //["{Spaces}/{4}", "{Spaces}/{2}"],
-  //["{Spaces}/{2}", "{Spaces}/{4}"],
-  //["{Spaces}/{2}", "{Tabs}/{1}"],
+  ["{Spaces}/{4}", "{Spaces}/{2}"],
+  ["{Spaces}/{2}", "{Spaces}/{4}"],
+  ["{Spaces}/{2}", "{Tabs}/{1}"],
   ["{Tabs}/{1}", "{Spaces}/{2}"],
 ];
+
+/** Regex to match the notation string */
 const regex = /\{(.+)\}\/\{(\d+)\}/;
+
 test.each(cases)("Test: From %p to %p", (from, to) => {
   const fromStr = SOURCE.get(from);
-  if (!fromStr) throw new Error("`from` key doesn't exist in SOURCE: " + from);
-
+  if (!fromStr) throw new Error(`"from" key ${from} doesn't exist in SOURCE`);
   const fromParts = regex.exec(from);
-  if (!fromParts) throw new Error("Invalid `from`: " + from);
+  if (!fromParts) throw new Error("Invalid `from` key: " + from);
 
-  const [_, sourceIndentStr, sourceIndentSizeStr] = fromParts;
+  const [sourceIndentStr, sourceIndentSizeStr] = [fromParts[1], fromParts[2]];
   const sourceIndent = sourceIndentStr as unknown as Indent;
   const sourceIndentSize = parseInt(sourceIndentSizeStr);
 
   const toStr = SOURCE.get(to);
-  if (!toStr) throw new Error("`to` key doesn't exist in SOURCE: " + to);
+  if (!toStr) throw new Error(`"to" key ${to} doesn't exist in SOURCE`);
   const toParts = regex.exec(to);
-  if (!toParts) throw new Error("Invalid `to`: " + to);
+  if (!toParts) throw new Error("Invalid `to` key: " + to);
 
-  const [__, requiredIndentStr, requiredIndentSizeStr] = toParts;
+  const [requiredIndentStr, requiredIndentSizeStr] = [toParts[1], toParts[2]];
   const requiredIndent = requiredIndentStr as unknown as Indent;
   const requiredIndentSize = parseInt(requiredIndentSizeStr);
 
