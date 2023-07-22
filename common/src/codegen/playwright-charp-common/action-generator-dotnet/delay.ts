1b20802cb67d71b3fc8ecbce10a59ed871ebb4f2
Võ Thái Sơn <127235746+vo-thaison@users.noreply.github.com>
2023-07-22 19:35:13 +0700

    Fix error in `delay` action

--- common/src/codegen/playwright-charp-common/action-generator-dotnet/delay.ts
+++ common/src/codegen/playwright-charp-common/action-generator-dotnet/delay.ts
@@ -3,9 +3,10 @@ import { createEnvironmentVariableString } from "../../utils/stringUtils";
 
 /** Generates Csharp code for action Delay */
 export default (params: IActionTemplateParam) => {
+  const envVarString = createEnvironmentVariableString(String(params.data.rawData));
   const data =
     params.data.dataType === ActionDataType.LiteralValue
       ? parseInt(String(params.data.rawData)) || 0
-      : `Int32.Parse(${createEnvironmentVariableString(String(params.data.rawData))})`;
+      : `Int32.Parse(string.IsNullOrEmpty(${envVarString}) ? "0" : ${envVarString})`;
   return `await Task.Delay(${data});`;
 };
